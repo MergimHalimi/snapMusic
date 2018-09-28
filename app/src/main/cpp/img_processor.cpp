b086ffba6d04a45c9da49e83a00a37d8294d5ebc
Mergim Halimi <m.halimi@abidat.de>
2018-09-28 11:44:09 +0200

    Added dataset .sh file, created functions to detect and recognise using YOLO algorithm.

--- app/src/main/cpp/img_processor.cpp
+++ app/src/main/cpp/img_processor.cpp
@@ -20,7 +20,7 @@ bool ImageProcessor::initialize(cv::Mat image)
   }
 }
 
-bool ImageProcessor::runProccessor(void)
+bool ImageProcessor::runProcessor(void)
 {
   if (!this->is_initialized_)
   {
@@ -30,7 +30,7 @@ bool ImageProcessor::runProccessor(void)
   else
   {
     //run the processor
-    processor(this->image_);
+    this->processor(this->image_);TODO:
     this->is_running_ = true;
     return true;
   }
@@ -48,19 +48,18 @@ bool ImageProcessor::isValid(void)
 std::vector<std::string> ImageProcessor::getData(void)
 {
 
-  if (!isValid())
+  if (!this->isValid())
   {
     this->image_data_.clear();
-    this->image_data_.push_back("Not initialized, or the processor not running");
+    this->image_data_.push_back("Not initialized, the processor not running or\nPermisions not allowed");
   }
   else
   {
-    this->image_data_.clear();
-    this->image_data_.push_back("Chair");
-    this->image_data_.push_back("Car");
-    this->image_data_.push_back("Test_1");
-    this->image_data_.push_back("Test_2");
-    this->image_data_.push_back("Test_3");
+    //this->image_data_.clear();
+    //this->image_data_.push_back("Chair");
+    //this->image_data_.push_back("Car");
+    //this->image_data_.push_back("Laptop");
+
   }
 
   return this->image_data_;
@@ -72,7 +71,126 @@ ImageProcessor::~ImageProcessor(void)
 }
 ///////////////////
 
-void ImageProcessor::processor(cv::Mat image)
+void ImageProcessor::processor(cv::Mat& image)
 {
-  //
+  this->initYOLOParams();
+
+  this->loadYOLOModel();
+
+  this->loadImage(image);
+
+  //Process frame
+
+}
+
+bool ImageProcessor::initYOLOParams(void)
+{
+  this->conf_threshold_ = 0.24;
+  this->nms_threshold_ = 0.4;
+  this->input_width_ = 416;
+  this->input_height_ = 416;
+
+  return true;
+}
+
+bool ImageProcessor::loadYOLOModel(void)
+{
+  // Load names of classes
+  this->classes_file_ = "/storage/emulated/0/datasets/coco.names";
+  std::ifstream ifs(classes_file_.c_str());
+  std::string line;
+
+  while (std::getline(ifs, line))
+  {
+    classes_.push_back(line);
+  }
+
+  //Give the configuration and weight files for the model
+  this->model_configuration_ = "/storage/emulated/0/datasets/yolov3.cfg";
+  this->model_weights_ = "/storage/emulated/0/datasets/yolov3.weights";
+
+  //Load the network
+  this->net_ = cv::dnn::readNetFromDarknet(this->model_configuration_, this->model_weights_);
+  this->net_.setPreferableBackend(cv::dnn::DNN_BACKEND_OPENCV);
+  this->net_.setPreferableTarget(cv::dnn::DNN_TARGET_CPU);
+
+  return true;
+}
+
+bool ImageProcessor::loadImage(cv::Mat& image)
+{
+  cv::Mat blob;
+  // Create a 4D blob from a frame.
+  blob = cv::dnn::blobFromImage(image, 1/255.0, cv::Size(this->input_width_, this->input_height_), cv::Scalar(0,0,0), true, false);
+
+  //Sets the input to the network
+  this->net_.setInput(blob);
+
+  this->detections = this->net_.forward();
+
+  this->filterWithConfidence();
+
+  return true;
+}
+
+bool ImageProcessor::filterWithConfidence()
+{
+  this->image_data_.clear();
+
+  for (int i = 0; i < this->detections.rows; i++)
+  {
+    const int probability_index = 5;
+    const int probability_size = this->detections.cols - probability_index;
+    double *prob_array_ptr = &this->detections.at<double>(i, probability_index);
+
+    size_t objectClass = std::max_element(prob_array_ptr, prob_array_ptr + probability_size) - prob_array_ptr;
+
+    float confidence = this->detections.at<float>(i, (int) objectClass + probability_index);
+
+    if (confidence >= this->conf_threshold_)
+    {
+      std::string className = objectClass < classes_.size() ?
+                              classes_[objectClass] :
+                              "CLASSES::UNCLASSIFIED";
+
+
+      std::string _buff = "Class: " + className + ", Confidence: " + std::to_string(confidence);
+
+      this->image_data_.push_back(_buff);
+    }
+  }
+
+  return true;
+}
+
+std::vector<cv::String> ImageProcessor::writeOutputsNames(const cv::dnn::Net& net)
+{
+  static std::vector<cv::String> names_;
+
+  this->image_data_.clear();
+  names_.clear();
+
+  this->image_data_.push_back("From img_processor : ");
+
+  //Get the indices of the output layers, i.e. the layers with unconnected outputs
+  std::vector<int> outLayers = net.getUnconnectedOutLayers();
+
+  //get the names of all the layers in the network
+  std::vector<cv::String> layers_names_ = net.getLayerNames();
+
+  // Get the names of the output layers in names
+  names_.resize(outLayers.size());
+
+  for (size_t i = 0; i < outLayers.size(); ++i)
+  {
+    this->image_data_.push_back(layers_names_[outLayers[i] - 1]);
+    names_[i] = layers_names_[outLayers[i] - 1];
+  }
+
+  if (names_.empty())
+  {
+    this->image_data_.push_back("No objects detected");
+  }
+
+  return names_;
 }
--- app/src/main/cpp/include/img_processor.h
+++ app/src/main/cpp/include/img_processor.h
@@ -1,72 +1,135 @@
 #ifndef ___IMAGE_PROCESSOR___
 #define ___IMAGE_PROCESSOR___
 
+//C++
+#include <fstream>
 #include <string>
-#include <opencv2/core.hpp>
+#include <vector>
 
+//OpenCV
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/dnn.hpp>
+#include <opencv2/dnn/shape_utils.hpp>
+
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/utils/trace.hpp>
+
+/**
+ * \brief This class holds the functionality to get the image, process it, and return a vector of strings as a result of the objects recognised
+ *        from the class. The algorithms used is YOLOV3 (You Only Look Once) and uses a pre trained weights and classes model.
+ */
 class ImageProcessor
 {
 public:
-    /**
-     * \brief ImageProcessor Class Constructor
-     */
-    ImageProcessor(void);
+  /**
+   * \brief ImageProcessor Class Constructor
+   */
+  ImageProcessor(void);
 
-    //Avoid copy
-    ImageProcessor(const ImageProcessor&) = delete;
-    //Allow move
-    ImageProcessor(ImageProcessor&&) = default;
+  //Avoid copy
+  ImageProcessor(const ImageProcessor&) = delete;
+  //Allow move
+  ImageProcessor(ImageProcessor&&) = default;
 
-    /**
-     * \brief ImageProcessor Class initializer
-     * \param image : cv::Mat - Image
-     * \return bool - True or false
-     */
-    bool initialize(cv::Mat image);
+  /**
+   * \brief ImageProcessor Class initializer
+   * \param image : cv::Mat - Image
+   * \return bool - True or false
+   */
+  bool initialize(cv::Mat image);
 
-    /**
-     * \brief ImageProcessor Class initializer
-     * \param image : cv::Mat - Image
-     * \return bool - True or false
-     */
-    bool runProccessor(void);
+  /**
+   * \brief ImageProcessor Class initializer
+   * \param image : cv::Mat - Image
+   * \return bool - True or false
+   */
+  bool runProcessor(void);
 
-    /**
-     * \brief ImageProcessor Class initializer
-     * \param image : cv::Mat - Image
-     * \return bool - True or false
-     */
-    bool isValid(void);
+  /**
+   * \brief ImageProcessor Class initializer
+   * \param image : cv::Mat - Image
+   * \return bool - True or false
+   */
+  bool isValid(void);
 
-    /**
-     * \brief ImageProcessor Class result getter method.
-     * \return std::vector<std::string> - True or false
-     */
-    std::vector<std::string> getData(void);
+  /**
+   * \brief ImageProcessor Class result getter method.
+   * \return std::vector<std::string> - True or false
+   */
+  std::vector<std::string> getData(void);
 
-    /**
-     * \brief ImageProcessor Class Destructor
-     */
-    ~ImageProcessor(void);
+  /**
+   * \brief ImageProcessor Class Destructor
+   */
+  ~ImageProcessor(void);
 private:
-    /**
-     * \brief ImageProcessor Class Destructor
-     */
-    void processor(cv::Mat image);
+  /**
+   * \brief ImageProcessor Class Destructor
+   */
+  void processor(cv::Mat& image);
 
-    /**
-     * \param image_ : cv::Mat -
-     * \param image_data_ : std::vector<std::string> -
-     * \param image_ : cv::Mat -
-     * \param image_ : cv::Mat -
-     * \param image_ : cv::Mat -
-     * \param image_ : cv::Mat -
-     * \param image_ : cv::Mat -
-     */
-    cv::Mat image_;
-    std::vector<std::string> image_data_;
-    bool is_initialized_ = false;
-    bool is_running_ = false;
+  /**
+   * \brief ImageProcessor Class function initYOLOParams, to initialize the parameters for the image processor
+   */
+  bool initYOLOParams(void);
+
+  /**
+  * \brief ImageProcessor Class function loadYOLOModel, to load the pretrained weights and model, and classes for image recognition.
+  */
+  bool loadYOLOModel(void);
+
+  /**
+  * \brief ImageProcessor Class function loadImage, to load the image and prepare for the postprocess (Final process of the recognition)
+  */
+  bool loadImage(cv::Mat& image);
+
+  /**
+  * \brief ImageProcessor Class function filterWithConfidence, the final process of image recognition and result writing (Remove results with low confidence)
+  */
+  bool filterWithConfidence();
+
+  /**
+  * \brief ImageProcessor Class function loadYOLOModel, to load the pretrained weights and model, and classes for image recognition.
+  */
+  std::vector<cv::String> writeOutputsNames(const cv::dnn::Net& net);
+
+  /**
+   * \param image_ : cv::Mat - The variable to store the input image
+   * \param image_data_ : std::vector<std::string> - The variable to store the output result as vector of strings
+   * \param is_initialized_ : bool -
+   * \param is_running_ : bool -
+   *
+   * \param conf_threshold_ : float - Confidence threshold
+   * \param nms_threshold_ : float - Non-maximum suppression threshold
+   * \param input_width_ : int - Width of network's input image
+   * \param input_height_ : int - Height of network's input image
+   * \param model_configuration_ : std::string -
+   * \param model_weights_ : std::string -
+   * \param classes_ : std::vector<std::string> -
+   * \param net_ : cv::dnn::Net -
+   * \param input_height_ : int -
+   * \param input_height_ : int -
+   * \param input_height_ : int -
+   * \param input_height_ : int -
+   * \param input_height_ : int -
+   * \param input_height_ : int -
+   */
+  cv::Mat image_;
+  std::vector<std::string> image_data_;
+  bool is_initialized_ = false,
+       is_running_ = false;
+
+  float conf_threshold_,
+        nms_threshold_;
+  int input_width_,
+      input_height_;
+  std::string classes_file_;
+  cv::String model_configuration_,
+             model_weights_;
+  std::vector<std::string> classes_;
+  cv::dnn::Net net_;
+  cv::Mat detections;
 };
-
 #endif //___IMAGE_PROCESSOR___
